a812b14489582c8dac83579674a0caa0672c5542
tomasnik <tomas@nevrlka.com>
2020-04-04 19:11:17 +0200

    reseni Vrahovy vypovedi

--- VrahovaVypoved/main.cpp
+++ VrahovaVypoved/main.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    // Open files
-    ifstream source("VYPOVED.TXT");
-    ofstream file("SKUTECNE.TXT");
-
-    // Read source
-    char text[999];
-    if (source.is_open())
+    // Open input
+    ifstream input("VYPOVED.TXT");
+    ofstream output("SKUTECNE.TXT");
+    // Read input
+    string text;
+    while (getline(input, text))
     {
-       for (int i = 0; ((text[i] = source.get()) != EOF); i++);
-    }
-    // Write to file
-    for (int i = 0; text[i] != EOF; i++)
-    {
-        if (text[i] == ' ' && text[i + 1] == 'n' && text[i + 2] == 'e')
+        for (int i = 0; i < text.length(); i++)
         {
-            file << text[i];
-            i += 3;
+            // If 'ne' is on the beginning of a line
+            if ((i == 0 && text[i] == 'n' && text[i + 1] == 'e'))
+            {
+                i += 2;
+            }
+            // If 'ne' is after a whitespace
+            else if (text[i] == ' ' && text[i + 1] == 'n' && text[i + 2] == 'e')
+            {
+                output << text[i];
+                i += 3;
+            }
+            // Write text
+            output << text[i];
         }
-        file << text[i];
+        // Linebreak
+        output << endl;
     }
-    source.close();
-    file.close();
+    input.close();
+    output.close();
     return 0;
 }
