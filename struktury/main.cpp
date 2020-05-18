0f5d2b2f365d3a3c9d4155fb63905bbebf68c975
tomasnik <tomas@nevrlka.com>
2020-05-18 12:11:16 +0200

    definovana struct ZAMEST

--- struktury/main.cpp
+++ struktury/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+struct ZAMEST {
+    char prijmeni[30];
+    char jmeno[30];
+    int plat;
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
